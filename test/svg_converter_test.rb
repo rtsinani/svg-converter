c7d2c0525408739ae967c3d97d3798345aac8657
Artan Sinani <artisinani@gmail.com>
2011-12-30 18:48:26 +0000

    change file paths in tests

--- test/svg_converter_test.rb
+++ test/svg_converter_test.rb
@@ -2,19 +2,21 @@ require 'test_helper'
 
 class SvgConverterTest < ActiveSupport::TestCase
   setup do
+    # Path to the batik jar
     SvgConverter.batik = '/Users/panjon/dev/batik/batik-1.7/batik-rasterizer.jar'
-    SvgConverter.temp_dir = '/Users/panjon/ror/svg_converter/tmp'
-    @svg_filename = "/Users/panjon/ror/svg_converter/test/samples/areaspline.svg"
+    current_dir = File.dirname(__FILE__)
+    SvgConverter.temp_dir = current_dir.gsub('/test', '/tmp')
+    @svg_filename = File.join(current_dir, 'samples/areaspline.svg')
   end
   
-  ['image/png', 'image/jpg'].each do |mime_type|
-    test "can convert to #{mime_type}" do
-      svg = File.new(@svg_filename).readlines
-      convertion = SvgConverter::Convertion.new(svg, mime_type, 200)
-      result = convertion.perform
-      assert_match 'success', result['output']
-      assert File.exists?(result['filename'])
-    end
-  end
+  ['image/png'].each do |mime_type|
+     test "can convert to #{mime_type}" do
+       svg = File.new(@svg_filename).readlines
+       convertion = SvgConverter::Convertion.new(svg, mime_type, 200)
+       result = convertion.perform
+       assert_match 'success', result['output']
+       assert File.exists?(result['filename'])
+     end
+   end
   
 end
